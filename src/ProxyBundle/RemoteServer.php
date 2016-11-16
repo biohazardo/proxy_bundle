54388a1ea846b4f4ba005848a9843cb79c6a6a1c
Nikita Kuhta <nikitakuhta@gmail.com>
2016-11-16 23:46:48 +0300

    Curl added

--- src/ProxyBundle/RemoteServer.php
+++ src/ProxyBundle/RemoteServer.php
@@ -23,13 +23,27 @@ class RemoteServer
         // По следующим параметрам определяет параметры будущего запроса
         $requestData = $this->router->getRequestParams($path, $method, $params);
         if ($requestData) {
-//            $response = $this->makeRequest($requestData->url, $requestData->method, $requestData->params);
+            $response = $this->makeRequest($requestData['url'], $requestData['method'], $requestData['params']);
+            return $response;
         } else {
             // нет никакого роута для этого запроса, ничего не надо вызывать
             return null;
         }
+    }
 
-        return 'fake result';
-
+    public function makeRequest($url, $method, $params) {
+        if ($method === 'GET') {
+            $url .= '?' . http_build_query($params);
+        }
+        $ch = curl_init($url);
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        }
+        if (! $response = curl_exec($ch)) {
+            throw new \Exception(curl_error($ch));
+        }
+        curl_close($ch);
+        return $response;
     }
 }
--- src/ProxyBundle/Router.php
+++ src/ProxyBundle/Router.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ProxyBundle;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -17,16 +18,38 @@ class Router
     }
 
     public function getRequestParams(string $path, string $method, array $params):array {
-        $url = $this->getRelevantUrl($path, $method, $params);
-        return [
-            'url' => '',
-            'method' => $method,
-            'params' => $params
-        ];
-    }
-
-    protected function getRelevantUrl(string $path, string $method, array $params) : string {
-        print_r($this->routes);
-        return '';
+        foreach($this->routes as $route) {
+            // путь должен быть заполнени, либо регулярное выражение
+            if (empty($route['regexp']) && empty($route['path'])) {
+                throw new \Exception("Need to defined regexp of path for route");
+            }
+            // должен быть адрес, либо же middleware
+            if (empty($route['middleware']) && empty($route['address'])) {
+                throw new \Exception("Need to define address or middleware for route");
+            }
+            $relevant = false; // признак подходящего маршрута
+            if (!empty($route['path']) && $path === $route['path']) {
+                $relevant = true;
+            } else if (!empty($route['regexp'])) {
+                throw new \Exception('Regexp not implemented now');
+            }
+            if ($relevant) {
+                // Маршрут подходит
+                $address = $route['address'];
+                if (!empty($address)) {
+                    return [
+                        'url' => $address,
+                        'method' => $method,
+                        'params' => $params
+                    ];
+                } else if (!empty($route['middleware'])) {
+                    // middleware модифицирует параметры
+                    $className = $route['middleware'];
+                    $middleware = new $className();
+                    return $middleware->makeRequest($path, $method, $params); // middleware подготовит данные
+                }
+            }
+        }
+        return null; // не подшел ни один маршрут
     }
 }
